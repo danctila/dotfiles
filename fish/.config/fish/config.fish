5fc96fc5d6fcd8c828f8f9be82d3f37d99764b3c
Dylan Anctil <134968796+danctila@users.noreply.github.com>
2026-05-17 16:10:37 -0400

    Add initial dotfiles

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,21 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/anaconda3/bin/conda
+    eval /opt/anaconda3/bin/conda "shell.fish" hook $argv | source
+else
+    if test -f "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH /opt/anaconda3/bin $PATH
+    end
+end
+# <<< conda initialize <<<
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+fish_add_path ~/go/bin
+
+# Starship
+starship init fish | source
